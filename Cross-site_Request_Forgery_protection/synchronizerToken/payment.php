64177a0199cee1fd8cb1d5ed90f5237d77ad89d2
LakshikaBandara <harshani.bandara2015@gmail.com>
2018-09-19 22:16:03 +0530

    completed synchronizer token pattern

--- Cross-site_Request_Forgery_protection/synchronizerToken/payment.php
+++ Cross-site_Request_Forgery_protection/synchronizerToken/payment.php
@@ -31,6 +31,18 @@
 	<link href="style.css" rel="stylesheet" id="bootstrap-css">
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script>
+	$(document).ready(function()
+	{
+		$.ajax({
+            type: "GET",
+            url: 'csrf_token_generate.php',
+            success: function (output) {
+                $('#csrf_token').val(output);                  
+            }
+        });
+	});
+	</script>
 	<title>Cross-site Request Forgery protection with Synchronizer Token Pattern</title>
 </head>
 <body>
@@ -60,7 +72,7 @@
 					<input type="text" name="branch" class="form-control" placeholder="Branch" required  style="background-color: #f2f5f2">
 				</div>
 				<button type="submit" class="btn btn-primary">Submit</button>				
-    			<input type="hidden" name="csrf_token" value="" id="csrf_token_hidden"/>	
+    			<input type="hidden" name="csrf_token" value="" id="csrf_token"/>	
 			</form>
 		</div>		
 	</div>
